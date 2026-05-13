eb373543200f970d1a208935cb15e7f041beabc7
Tango-Mik <tariq.manzoor@live.com>
2026-05-13 22:38:59 +0400

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,17 +1,13 @@
 import requests
+import os
 
-# ✅ Paste your NEW bot token between the quotes
-BOT_TOKEN = "8626046136:AAHa-v95IDRzXJ2Zpbkf_-d_QzXBfc4KoJY"
-
-# ✅ Paste your chat ID between the quotes
-CHAT_ID = "1089668720"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def notify(message):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot8626046136:AAHa-v95IDRzXJ2Zpbkf_-d_QzXBfc4KoJY/sendMessage"
     payload = {
         "chat_id": CHAT_ID,
         "text": message
     }
     requests.post(url, data=payload)
-
-# ✅ TEMPORARY TEST MESSAGE
