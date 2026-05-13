9c84f37cbb00531804d108a7dbdb697e2ba5c181
Tango-Mik <tariq.manzoor@live.com>
2026-05-13 22:46:13 +0400

    Update check_vfs.py

--- check_vfs.py
+++ check_vfs.py
@@ -7,7 +7,7 @@ URLS = {
 
 def check_vfs(country):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto(URLS[country], wait_until="domcontentloaded", timeout=60000)
